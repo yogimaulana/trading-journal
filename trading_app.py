e0588748a03eb4f61c983915ab9ac0692e3075a5
yogimaulana <49883385+yogimaulana@users.noreply.github.com>
2026-08-28 04:05:04 +0700

    Update trading_app.py

--- trading_app.py
+++ trading_app.py
@@ -18,17 +18,15 @@ st.set_page_config(
 )
 
 # ==================== KUSTOM CSS & RESPONSIF OTOMATIS (HP & PC) ====================
-st.markdown("""
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-75YJERE40G"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-75YJERE40G"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-      gtag('config', 'G-75YJERE40G');
-    </script>
-""", unsafe_allow_html=True)
+  gtag('config', 'G-75YJERE40G');
+</script>
 st.markdown("""
     <style>
     .stApp {
